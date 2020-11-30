697d733c01e369c0b877cedda5c16dcee03e03c2
TrungNhanNguyenHuu <nhan.nguyenbkedu@hcmut.edu.vn>
2020-11-30 12:16:48 +0700

    commit_4th; Victory file Header (html and css) Victory file Footer (html and css) Victory file NavBar (html and css) Victory file HomePage (html and css) ;

---
+++ controller/index.php
@@ -0,0 +1,6 @@
+<?php
+  include '../view/markup_Header.html';
+  include_once '../view/markup_NavBar.html';
+  include_once '../view/markup_HomePage.html';
+  include '../view/markup_Footer.html';
+?>
