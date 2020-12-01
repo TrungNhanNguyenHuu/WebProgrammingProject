a24e0f17acdbe6608812076834bfa19f23451e4c
TrungNhanNguyenHuu <nhan.nguyenbkedu@hcmut.edu.vn>
2020-12-01 21:19:38 +0700

    commit_6th; edit index.php file; add Login page (html , css, js, php file);

---
+++ controller/controller_Login.php
@@ -0,0 +1,5 @@
+<?php
+  printf("<p>UserName: %s </p>", $_POST['Login_userName']);
+  printf("<p>PassWord: %s </p>", $_POST['Login_passWord']);
+  printf("<p>UserType: %s </p>", $_POST['opUser']);
+?>
--- controller/index.php
+++ controller/index.php
@@ -1,4 +1,6 @@
 <?php
+  session_start();
+
   include '../view/markup_Header.html'; // Always include the header for all pages
   
   if (isset($_GET['page'])) { // Method GET case
@@ -15,74 +17,6 @@
 
   include '../view/markup_Footer.html'; // Always include the footer for all pages
 ?>
-
-<!--
-  if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-    if ($page == "index" or $page == "document_1_mainPage" or $page == "document_3_login") {
-      include "$page.html";
-    } else {
-      include "$page.php";
-    }
-  } else if (isset($_POST['page'])) {
-    $page = $_POST['page'];
-    include "$page.php";
-  }
--->
-<!--
-    session_start();
-
-    include '../../secret/config.php';
-    include "./common/autoAuthen.php";
-
-    $page_content = array();
-
-    AutoAuthen::rememberMe();
-
-
-
-    array_push($page_content , "../views/header.html");
-    if (isset($_SESSION['user_type'])){
-        $nav = $_SESSION['user_type'];
-        array_push($page_content ,"../views/navbar_$nav.html");
-    } else {
-        array_push($page_content , "../views/navbar.html");
-    }
-
-
-    $page = isset($_GET['page']) ? $_GET['page']: '';
-
-    switch(true) {
-        case !isset($_GET['page']):
-            header("Location: ".Config::getConfig()['domain'] ."bodyBanner");
-            break;
-
-        case (!isset($_SESSION['user_type']) && in_array($_GET['page'],Config::getPermission()['common'])):
-            array_push($page_content , "../views/$page.html");
-            break;
-        case (!isset($_SESSION['user_type']) && !in_array($_GET['page'],Config::getPermission()['common'])):
-            header("Location: ".Config::getConfig()['domain'] ."formLogin");
-            break;
-        case in_array($page, Config::getPermission()['loginCommon']):
-        case in_array($page, Config::getPermission()[$_SESSION['user_type']]):
-            array_push($page_content , "../views/$page.html");
-            break;
-        default:
-            header("Location: ".Config::getConfig()['domain'] ."bodyBanner");
-    }
-
-    if (in_array($_GET['page'],Config::get()['pageGoWithCouting'])){
-        array_push($page_content , "../views/counting.html");
-    }
-    
-    array_push($page_content , "../views/footer.html");
-
-    foreach ($page_content as $value) {
-        include_once $value;
-    }
-*/
-  -->
-
   <!--
         $username1 = $_POST['username'];
     $password1 = $_POST['password'];
